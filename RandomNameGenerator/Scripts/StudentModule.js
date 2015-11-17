b95c891056674fbcbd2e04348a3c4b39dfe3e0bd
samjhooker <samjhooker@gmail.com>
2015-11-17 15:42:14 +1300

    created a Javascript/jquery file to call the api

---
+++ RandomNameGenerator/Scripts/StudentModule.js
@@ -0,0 +1,29 @@
+﻿
+//this module will make the http requests
+
+var StudentModule = (function () {
+    
+    return{
+        getStudents: function (){
+            //get studetns code here
+            $.ajax({
+                type: "GET",
+                data: "jsonp",
+                url: "http://api.uinames.com/?amount=25",
+                success: function(data){
+                    console.log(data);
+                }
+
+            });
+
+
+        },
+        deleteStudents: function () {
+            //insert delete students
+        },
+        editStudents: function (){
+            // insert edit coed here
+        }
+    }
+
+}());
